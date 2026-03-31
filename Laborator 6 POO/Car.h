a03a4b45803a2bdb35e757eb7c7a841b5e346022
gheorghitamarian <eusebiupsn@gmail.com>
2026-03-31 22:24:37 +0300

    Add files via upload

---
+++ Laborator 6 POO/Car.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Weather.h"
+
+class Car {
+public:
+    virtual ~Car() = default;
+    virtual double GetFuelCapacity() const = 0;
+    virtual double GetFuelConsumption() const = 0;
+    virtual double GetSpeed(Weather weather) const = 0;
+    virtual const char* GetName() const = 0;
+};
